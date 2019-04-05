5f370a06c44d2e52ec15f3a46aa1d4577ac5c654
Silvan Knecht <knecht.silvan@gmail.com>
2019-04-05 19:56:41 +0200

    snake server

--- public/snake/main.js
+++ public/snake/main.js
@@ -1,113 +1,53 @@
 var c = document.getElementById("snakeCanvas");
 var ctx = c.getContext("2d");
-
-let HEIGHT = c.height;
-let WIDTH = c.width;
+const WIDTH = 600;
+const HEIGHT = 600;
 const BODYHIGHT = 15;
 const BODYWIDTH = 15;
 
-let movementX = 0;
-let movementY = 0;
-let gameRunning = false;
-let score = 0;
-
-var activeRegion = ZingTouch.Region(c);
-new ZingTouch.Swipe({
-  numInputs: 1,
-  maxRestTime: 20,
-  escapeVelocity: 0.1
+var socket = io('http://localhost:3000');
+socket.on('connect', function () {
+  console.log('Connected to Server!')
 });
-activeRegion.bind(
-  c,
-  "swipe",
-  function(e) {
-    let direction = e.detail.data[0].currentDirection;
+socket.on('event', function (data) {});
+socket.on('disconnect', function () {});
 
-    if (direction < 225 && direction > 135 && movementX !== 1) {
-      gameRunning = true;
-      movementX = -1;
-      movementY = 0;
-    } else if (direction < 135 && direction > 45 && movementY !== 1) {
-      gameRunning = true;
-      movementY = -1;
-      movementX = 0;
-    } else if (
-      (direction < 45 || direction > 315) &&
-      direction >= 0 &&
-      movementX !== -1
-    ) {
-      gameRunning = true;
-      movementX = 1;
-      movementY = 0;
-    } else if (direction < 315 && direction > 225 && movementY !== -1) {
-      {
-        gameRunning = true;
-        movementY = 1;
-        movementX = 0;
-      }
-    }
-    console.log(e.detail.data[0].currentDirection);
-  },
-  false
-);
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, WIDTH, HEIGHT);
 ctx.stroke();
 
-let snake = [];
 
-function createBodyPart(x, y) {
-  snake.unshift({ x, y });
-}
-
-let food = { x: null, y: null, eaten: false };
-
-function createFood() {
-  if (food.x === null) {
-    let randX = Math.floor(Math.random() * (WIDTH / BODYWIDTH - 1)) * BODYWIDTH;
-    let randY =
-      Math.floor(Math.random() * (HEIGHT / BODYHIGHT - 1)) * BODYHIGHT;
-    for (let bp in snake) {
-      let { x, y } = bp;
-      if (x === randX && y === randY) {
-        createFood();
-      }
-    }
-    food.x = randX;
-    food.y = randY;
-  }
-}
-createFood();
-
-createBodyPart(WIDTH / 2, HEIGHT / 2);
-
-window.setInterval(function() {
+socket.on('move', (clients) => {
+  console.log(clients);
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
   ctx.stroke();
 
-  gameover();
-  drawBody();
-  if (food.x !== null) drawFood();
-  if (gameRunning) move();
-  eat();
+  for (let c of clients) {
+    drawBody(c.snake.body);
+  }
+  //if (food.x !== null) drawFood();
+  //if (gameRunning) move();
+  //eat();
   ctx.strokeStyle = "white";
   ctx.font = "30px Verdana";
-  ctx.strokeText(score, WIDTH - 50, 50);
-}, 50);
+  ctx.strokeText("score", WIDTH - 50, 50);
+});
 
-window.setInterval(function() {
-  gameover();
-}, 10);
 
-window.setInterval(function() {
-  createFood();
-}, 3000);
 
-function drawBody() {
-  for (let [i, bp] of snake.entries()) {
-    let { x, y } = bp;
+
+
+
+
+
+function drawBody(body) {
+  for (let [i, bp] of body.entries()) {
+    let {
+      x,
+      y
+    } = bp;
 
     //ctx.strokeStyle = "red";
     ctx.fillStyle = "white";
@@ -125,78 +65,25 @@ function drawFood() {
   ctx.strokeRect(food.x, food.y, BODYHIGHT, BODYWIDTH);
 }
 
-function move() {
-  let { x, y } = snake[0];
-  createBodyPart(x + BODYWIDTH * movementX, y + BODYWIDTH * movementY);
-  if (food.eaten === false) {
-    snake.pop();
-  } else {
-    food.eaten = false;
-  }
-}
 
-function eat() {
-  let { x, y } = snake[0];
-  if (x === food.x && y === food.y) {
-    food.eaten = true;
-    food.x = null;
-    food.y = null;
-    score++;
-  }
-}
 
-function gameover() {
-  let { x, y } = snake[0];
-  if (x > WIDTH || x < 0 || y > HEIGHT || y < 0) {
-    restart();
-  }
-  for (let i = 1; i < snake.length; i++) {
-    if (x === snake[i].x && y === snake[i].y) {
-      restart();
-    }
-  }
-}
 
-function restart() {
-  snake = [];
-  food = { x: null, y: null, eaten: false };
-  movementX = 0;
-  movementY = 0;
-  gameRunning = false;
-  score = 0;
-  createFood();
-  createBodyPart(WIDTH / 2, HEIGHT / 2);
-}
 
-document.onkeydown = function(e) {
+
+
+document.onkeydown = function (e) {
   switch (e.keyCode) {
     case 37: // left
-      if (movementX !== 1) {
-        gameRunning = true;
-        movementX = -1;
-        movementY = 0;
-      }
+      socket.emit("changeDirection", "left");
       break;
     case 38: // up
-      if (movementY !== 1) {
-        gameRunning = true;
-        movementY = -1;
-        movementX = 0;
-      }
+      socket.emit("changeDirection", "up");
       break;
     case 39: // right
-      if (movementX !== -1) {
-        gameRunning = true;
-        movementX = 1;
-        movementY = 0;
-      }
+      socket.emit("changeDirection", "right")
       break;
     case 40: // down
-      if (movementY !== -1) {
-        gameRunning = true;
-        movementY = 1;
-        movementX = 0;
-      }
+      socket.emit("changeDirection", "down");
       break;
   }
 };
--- server.js
+++ server.js
@@ -4,6 +4,244 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 const compression = require('compression');
+const server = require('http').createServer(app);
+
+const HEIGHT = 600;
+const WIDTH = 600;
+const BODYHIGHT = 15;
+const BODYWIDTH = 15;
+
+let gameRunning = false;
+let food = {
+  x: null,
+  y: null,
+  eaten: false
+};
+
+let clients = [];
+const io = require('socket.io')(server);
+io.on('connection', client => {
+  console.log("connected to server!");
+  let newClient = {
+    id: client.id,
+    snake: new Snake()
+  };
+  newClient.snake.createBodyPart(WIDTH / 2, HEIGHT / 2);
+  clients.push(newClient);
+  console.log(clients[0].snake.body);
+
+});
+
+class Snake {
+
+  constructor() {
+    this.mov = {
+      x: 0,
+      y: 0
+    };
+    this.body = [];
+    this.score = 0;
+    this.created = false;
+  }
+
+  createBodyPart(x, y) {
+    this.body.unshift({
+      x,
+      y
+    });
+  }
+}
+
+
+
+
+
+function createFood() {
+  if (food.x === null) {
+    let randX = Math.floor(Math.random() * (WIDTH / BODYWIDTH - 1)) * BODYWIDTH;
+    let randY =
+      Math.floor(Math.random() * (HEIGHT / BODYHIGHT - 1)) * BODYHIGHT;
+    for (let c in clients) {
+      let {
+        snake
+      } = c;
+      if (snake.posX === randX && snake.posY === randY) {
+        createFood();
+      }
+    }
+    food.x = randX;
+    food.y = randY;
+  }
+}
+createFood();
+
+
+setInterval(function () {
+  io.emit('move', clients);
+  // ctx.fillStyle = "black";
+  // ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  // ctx.stroke();
+
+  //gameover();
+  // drawBody();
+  // if (food.x !== null) drawFood();
+  // if (gameRunning) move();
+  // eat();
+  // ctx.strokeStyle = "white";
+  // ctx.font = "30px Verdana";
+  // ctx.strokeText(score, WIDTH - 50, 50);
+}, 50);
+
+
+io.on("changeDirection", (dir) => {
+  console.log(dir);
+
+  switch (dir) {
+    case "left": // left
+      if (movementX !== 1) {
+        gameRunning = true;
+        movementX = -1;
+        movementY = 0;
+      }
+      break;
+    case "up": // up
+      if (movementY !== 1) {
+        gameRunning = true;
+        movementY = -1;
+        movementX = 0;
+      }
+      break;
+    case "right": // right
+      if (movementX !== -1) {
+        gameRunning = true;
+        movementX = 1;
+        movementY = 0;
+      }
+      break;
+    case "down": // down
+      if (movementY !== -1) {
+        gameRunning = true;
+        movementY = 1;
+        movementX = 0;
+      }
+      break;
+  }
+});
+// setInterval(function() {
+//   gameover();
+// }, 10);
+
+// setInterval(function() {
+//   createFood();
+// }, 3000);
+
+// function drawBody() {
+//   for (let [i, bp] of snake.entries()) {
+//     let { x, y } = bp;
+
+//     //ctx.strokeStyle = "red";
+//     ctx.fillStyle = "white";
+//     ctx.fillRect(x, y, BODYWIDTH, BODYHIGHT);
+//     //ctx.stroke();
+//     if (i === 0) {
+//       ctx.fillStyle = "green";
+//       ctx.fillRect(x, y, BODYWIDTH, BODYHIGHT);
+//     }
+//   }
+// }
+
+// function drawFood() {
+//   ctx.strokeStyle = "red";
+//   ctx.strokeRect(food.x, food.y, BODYHIGHT, BODYWIDTH);
+// }
+
+// function move() {
+//   let { x, y } = snake[0];
+//   createBodyPart(x + BODYWIDTH * movementX, y + BODYWIDTH * movementY);
+//   if (food.eaten === false) {
+//     snake.pop();
+//   } else {
+//     food.eaten = false;
+//   }
+// }
+
+// function eat() {
+//   let { x, y } = snake[0];
+//   if (x === food.x && y === food.y) {
+//     food.eaten = true;
+//     food.x = null;
+//     food.y = null;
+//     score++;
+//   }
+// }
+
+// function gameover() {
+//   let { x, y } = snake[0];
+//   if (x > WIDTH || x < 0 || y > HEIGHT || y < 0) {
+//     restart();
+//   }
+//   for (let i = 1; i < snake.length; i++) {
+//     if (x === snake[i].x && y === snake[i].y) {
+//       restart();
+//     }
+//   }
+// }
+
+function restart() {
+  snake = [];
+  food = {
+    x: null,
+    y: null,
+    eaten: false
+  };
+  movementX = 0;
+  movementY = 0;
+  gameRunning = false;
+  score = 0;
+  createFood();
+  createBodyPart(WIDTH / 2, HEIGHT / 2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // middlewares
@@ -18,27 +256,30 @@ app.use(express.static('public'));
 
 // index
 app.get('/', function (req, res) {
-  let filePath = `/home/pi/Documents/data/ipadresses.txt`;
-  let stats = fs.statSync(filePath);
-  if (stats["size"] < 5000000) {
-    fs.appendFile(filePath, req.connection.remoteAddress + ", ", function (err, data) {
-      if (err) {
-        console.log(err);
-      }
+  // let filePath = `/home/pi/Documents/data/ipadresses.txt`;
+  // let stats = fs.statSync(filePath);
+  // if (stats.size < 5000000) {
+  //   fs.appendFile(filePath, req.connection.remoteAddress + ", ", function (err, data) {
+  //     if (err) {
+  //       console.log(err);
+  //     }
 
-    });
-  } else {
-    console.log("save file full");
-  }
+  //   });
+  // } else {
+  //   console.log("save file full");
+  // }
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/snake', function(req, res){
-  res.sendFile(path.join('./snake/index.html'));})
+app.get('/snake', function (req, res) {
+  res.sendFile(path.join('./snake/index.html'));
+})
 
 // routes
 app.use('/mail', mail);
 
 // add the router
-app.listen(process.env.port || 3000);
+server.listen(process.env.port || 3000);
 console.log('Running at Port 3000');
+
+module.exports.server = server;
